14b17ca4c659a30b2278e28f4c7c741dc9e361eb
dasgrossfuchs <agustinabta@gmail.com>
2020-11-20 00:42:41 -0800

    Mas madres de UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,6 +36,8 @@ namespace QR_Imagenes
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.botonguardarimagen = new System.Windows.Forms.Button();
+            this.dimension = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -77,7 +79,7 @@ namespace QR_Imagenes
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(22, 21);
+            this.panel1.Location = new System.Drawing.Point(22, 22);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(300, 300);
             this.panel1.TabIndex = 5;
@@ -87,6 +89,7 @@ namespace QR_Imagenes
             this.pictureBox1.Location = new System.Drawing.Point(617, 80);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
@@ -99,23 +102,61 @@ namespace QR_Imagenes
             this.botonguardarimagen.Text = "Guardar imagen";
             this.botonguardarimagen.UseVisualStyleBackColor = true;
             // 
+            // dimension
+            // 
+            this.dimension.FormattingEnabled = true;
+            this.dimension.Items.AddRange(new object[] {
+            "1 x 1",
+            "2 x 2",
+            "3 x 3",
+            "4 x 4",
+            "5 x 5",
+            "6 x 6",
+            "7 x 7",
+            "8 x 8",
+            "9 x 9",
+            "10 x 10",
+            "11 x 11",
+            "12 x 12",
+            "13 x 13",
+            "14 x 14",
+            "15 x 15",
+            "16 x 16"});
+            this.dimension.Location = new System.Drawing.Point(328, 37);
+            this.dimension.Name = "dimension";
+            this.dimension.Size = new System.Drawing.Size(67, 21);
+            this.dimension.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Location = new System.Drawing.Point(328, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Dimensiones";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dimension);
             this.Controls.Add(this.botonguardarimagen);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.botonguardar);
             this.Controls.Add(this.botoncargar);
             this.Controls.Add(this.botonsalir);
             this.Controls.Add(this.botonayuda);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -128,6 +169,8 @@ namespace QR_Imagenes
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button botonguardarimagen;
+        private System.Windows.Forms.ComboBox dimension;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -13,14 +13,98 @@ namespace QR_Imagenes
 {
     public partial class Form1 : Form
     {
+        int size;
+        string name;
+        string cadena;
+        string[,] pixel;
+        Color color;
+        PictureBox[,] pxl;
         public Form1()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            dimension.SelectedIndex = 15;
+            cuadricular();
+            color = ColorTranslator.FromHtml("#000000");
+            //Color c = new Color();
+            //c = ColorTranslator.FromHtml("#00000000") ;
+        }
+        public void cuadricular()
+        {
+            size = dimension.SelectedIndex + 1;
+            pixel = new string[size, size];
+            pxl = new PictureBox[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int ii = 0; ii < size; ii++)
+                {
+                    pixel[i, ii] = "t";
+                }
+            }
+            int w = panel1.Width / size;
+            int h = panel1.Height / size;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int ii = 0; ii < size; ii++)
+                {
+                    int x = panel1.Location.X + (w * ii);
+                    int y = panel1.Location.Y + (h * i);
+                    pxl[i,ii] = new PictureBox();
+                    pxl[i, ii].Size = new Size(w, h);
+                    pxl[i, ii].Location = new Point(x, y);
+                    pxl[i, ii].Name = "lbl" + (ii + i * 10);
+                    pxl[i, ii].BorderStyle = BorderStyle.FixedSingle;
+                    pxl[i, ii].SizeMode = PictureBoxSizeMode.StretchImage;
+                    this.Controls.Add(pxl[i,ii]);
+                }
+            }
+            panel1.SendToBack();
+            actualizardibujo();
+        }
+        public Image img(string value)
+        {
+            Bitmap imagen = new Bitmap(16,16);
+            Graphics grafico = Graphics.FromImage(imagen);
+            grafico.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+            if (value == "t")
+            {
+                grafico.FillRectangle(new SolidBrush(Color.Gray), 0, 0, 8, 8);
+                grafico.FillRectangle(new SolidBrush(Color.Gray), 8, 8, 8, 8);
+                grafico.FillRectangle(new SolidBrush(Color.White), 8, 0, 8, 8);
+                grafico.FillRectangle(new SolidBrush(Color.White), 0, 8, 8, 8);
+
+            }
+            else
+            {
+                grafico.FillRectangle(new SolidBrush(color), 0, 0, 15, 15);
+                grafico.DrawRectangle(new Pen(color), 0, 0, 15, 15);
+            }
+            return imagen;
+        }
+        public void actualizardibujo()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int ii = 0; ii < size; ii++)
+                {
+                    pxl[i, ii].Image = img(pixel[i,ii]);
+                    pxl[i, ii].Refresh();
+                }
+            }
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int ii = 0; ii < 10; ii++)
+            //    {
+            //        string name = "lbl" + (ii + i * 10);
+
+            //        Control ctn = this.Controls[name];
+
+            //        ctn.Text = procesos[(ii + i * 10)].ToString();
+            //    }
+            //}
         }
     }
 }
